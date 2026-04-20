f0fe42e118bb22f475032d0ab816abf8d8ad1494
trulsrane <truls.rane@gmail.com>
2026-04-20 09:22:03 +0200

    Added cta pattern

---
+++ patterns/cta-centered-heading.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Title: Call to Action – Centered Heading
+ * Slug: ihbi/cta-centered-heading
+ * Categories: call-to-action
+ * Description: Centered heading, subtext and a contact button for use as a page CTA section.
+ *
+ * @package IHBI_Lab_Theme
+ */
+?>
+
+<!-- wp:group {"align":"full","style":{"spacing":{"padding":{"right":"var:preset|spacing|40","left":"var:preset|spacing|40","top":"var:preset|spacing|70","bottom":"var:preset|spacing|70"},"margin":{"top":"0","bottom":"0"}},"dimensions":{"minHeight":"0vh"}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-group alignfull" style="min-height:0vh;margin-top:0;margin-bottom:0;padding-top:var(--wp--preset--spacing--70);padding-right:var(--wp--preset--spacing--40);padding-bottom:var(--wp--preset--spacing--70);padding-left:var(--wp--preset--spacing--40)">
+
+	<!-- wp:group {"style":{"spacing":{"blockGap":"var:preset|spacing|40"}},"layout":{"type":"constrained"}} -->
+	<div class="wp-block-group">
+
+		<!-- wp:heading {"textAlign":"center","style":{"spacing":{"margin":{"right":"0","left":"0"},"padding":{"right":"0","left":"0"}}},"fontSize":"h1"} -->
+		<h2 class="wp-block-heading has-text-align-center has-h-1-font-size" style="margin-right:0;margin-left:0;padding-right:0;padding-left:0"><?php esc_html_e( 'We are always looking for new collaborations!', 'ihbi-wp-theme' ); ?></h2>
+		<!-- /wp:heading -->
+
+		<!-- wp:paragraph {"align":"center"} -->
+		<p class="has-text-align-center"><?php esc_html_e( 'This is a subheading to give more information. Change it to suit the CTA title and page context, or remove it.', 'ihbi-wp-theme' ); ?></p>
+		<!-- /wp:paragraph -->
+
+		<!-- wp:buttons {"layout":{"type":"flex","justifyContent":"center"}} -->
+		<div class="wp-block-buttons">
+			<!-- wp:button -->
+			<div class="wp-block-button"><a class="wp-block-button__link wp-element-button" href="/contact"><?php esc_html_e( 'Get in contact', 'ihbi-wp-theme' ); ?></a></div>
+			<!-- /wp:button -->
+		</div>
+		<!-- /wp:buttons -->
+
+	</div>
+	<!-- /wp:group -->
+
+</div>
+<!-- /wp:group -->
